fa369dff54f97acb76c7aef49640cafd9c08e2e7
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2025-02-19 08:48:46 +0200

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,9 @@
+
+a = input("Say hi:")
+print(a)
+a1 = int(input("Enter first number: "))
+a2 = int(input("Enter second number: "))
+a3 = a1+a2
+print(a3)
+b = input("Enter your name: ")
+print("Hi! ", b)
